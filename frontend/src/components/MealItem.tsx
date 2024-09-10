c6573059f321876bb7f2d54132c7e1e0b904c344
alizade551 <hafiz.alizade93@gmail.com>
2024-09-10 16:40:02 +0400

    Adding currency formatter

--- frontend/src/components/MealItem.tsx
+++ frontend/src/components/MealItem.tsx
@@ -1,3 +1,5 @@
+import { currencyFormatter } from '../util/formatting';
+
 type Meal = {
   id: string;
   name: string;
@@ -17,7 +19,7 @@ function MealItem({ meal }: MealItemProps) {
         <img src={`http://localhost:3000/${meal.image}`} alt='' />
         <div>
           <h3>{meal.name}</h3>
-          <p className='meal-item-price'> {meal.price}</p>
+          <p className='meal-item-price'> {currencyFormatter.format(+meal.price)}</p>
           <p className='meal-item-description'> {meal.description}</p>
         </div>
         <p className='meal-item-actions'>
