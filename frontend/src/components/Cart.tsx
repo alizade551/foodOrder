6b6d3915cbe944fb38a80d15ba255336f07d77b3
alizade551 <hafiz.alizade93@gmail.com>
2024-09-11 01:54:05 +0400

    CartItem components

--- frontend/src/components/Cart.tsx
+++ frontend/src/components/Cart.tsx
@@ -3,9 +3,10 @@ import { useCart } from '../hooks/useCart';
 import { currencyFormatter } from '../util/formatting';
 import Button from './UI/Button';
 import { useProgress } from '../hooks/useProgress';
+import CartItem from '../../../../context/src/components/CartItem';
 
 function Cart() {
-  const { items } = useCart();
+  const { items, addItem, removeItem } = useCart();
   const { progress, hideCart } = useProgress();
   const cartTotalPrice: number = items.reduce((acc, item) => (acc += item.quantity * parseFloat(item.price)), 0);
 
@@ -18,9 +19,14 @@ function Cart() {
       <h2>Your cart</h2>
       <ul>
         {items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+          <CartItem
+            key={item.id}
+            name={item.name}
+            price={parseFloat(item.price)}
+            quantity={item.quantity}
+            onIncrease={() => addItem(item)}
+            onDecrease={() => removeItem(item.id)}
+          />
         ))}
       </ul>
       <p className='cart-total'>{currencyFormatter.format(cartTotalPrice)}</p>
