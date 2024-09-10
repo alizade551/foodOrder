cbebd711dc2235436b170d74293d510ccf55111b
alizade551 <hafiz.alizade93@gmail.com>
2024-09-10 15:35:37 +0400

    Meals component & fetching meals

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,11 @@
-import { useEffect } from 'react';
 import Header from './components/Header';
+import Meals from './components/Meals';
 
 function App() {
-  useEffect(() => {
-    fetch('http://localhost:3000/meals')
-      .then((res) => res.json())
-      .then(console.log);
-  }, []);
-
   return (
     <>
       <Header />
+      <Meals />
     </>
   );
 }
--- frontend/src/components/Meals.tsx
+++ frontend/src/components/Meals.tsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from 'react';
+
+type MealType = {
+  id: string;
+  name: string;
+  price: string;
+  description: string;
+  image: string;
+};
 function Meals() {
-  return <div>Meals</div>;
+  const [meals, setMeals] = useState<MealType[]>([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch('http://localhost:3000/meals', { method: 'GET' });
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      const data = await response.json();
+      setMeals(data);
+    };
+
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id='meals'>
+      {meals.map((meal: MealType) => (
+        <li key={meal.id}>{meal.name}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default Meals;
